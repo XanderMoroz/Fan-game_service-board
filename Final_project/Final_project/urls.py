ad2c30673e865bcd09518997b0e03d40ad9b05f0
Александр <7176uiop3436>
2022-08-23 20:09:54 +0300

    url for Advertisementsapp

--- Final_project/Final_project/urls.py
+++ Final_project/Final_project/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+   # Подключаем все адреса из приложения "advertisments" к главному приложению с префиксом ads/.
+   path('ads/', include('advertisements.urls')),
 ]
