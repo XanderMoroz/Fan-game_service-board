37e38f66d422db9f59336952bd0d9cc88e222e50
Александр <7176uiop3436>
2022-08-23 20:13:41 +0300

    add AdvertisementList as class of view

--- Final_project/advertisements/views.py
+++ Final_project/advertisements/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Advertisement
 # Create your views here.
+
+class AdvertisementList(ListView):
+    # Указываем модель, объекты которой мы будем выводить (Объявление)
+    model = Advertisement
+    # Поле, которое будет использоваться для сортировки объявлений
+    ordering = 'creation_date'
+    # Указываем имя шаблона, в котором будут все инструкции о том, как должны быть показаны объявления
+    template_name = 'ads_list.html'
+    # Слово, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'Advertisements'
