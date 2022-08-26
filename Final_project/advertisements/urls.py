e024e1a8b0883bce362a11ef45764b9e9d8bf767
Александр <7176uiop3436>
2022-08-27 00:31:18 +0300

    new_urls added crud in a process

--- Final_project/advertisements/urls.py
+++ Final_project/advertisements/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import AdvertisementList, AdvertisementDetail
+from .views import AdvertisementList, AdvertisementDetail, AdCreate
 
 urlpatterns = [
     # Представляем наш класс списка объявлений(AdvertisementList из view.py) при помощи метода as_view.
     path('', AdvertisementList.as_view(), name='ads_list'),
     # pk — это первичный ключ объявления, который будет выводиться у нас в шаблон
     # int — указывает на то, что принимаются только целочисленные значения
-    path('<int:pk>', AdvertisementDetail.as_view(), name='ad_detail')
+    path('<int:pk>', AdvertisementDetail.as_view(), name='ad_detail'),
+    path('ad_create', AdCreate.as_view(), name='create_advertisement'),
+    path('ad_update', AdUpdate.as_view(), name='update_advertisement')
     ]
