993fd6444550a0a11b843f769d6cacb19eb0b37e
Александр <7176uiop3436>
2022-08-27 20:22:18 +0300

    crud urls added

--- Final_project/advertisements/urls.py
+++ Final_project/advertisements/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AdvertisementList, AdvertisementDetail, AdCreate, AdUpdate
+from .views import AdvertisementList, AdvertisementDetail, AdCreate, AdUpdate, AdDelete
 
 urlpatterns = [
     # Представляем наш класс списка объявлений(AdvertisementList из view.py) при помощи метода as_view.
@@ -7,6 +7,8 @@ urlpatterns = [
     # pk — это первичный ключ объявления, который будет выводиться у нас в шаблон
     # int — указывает на то, что принимаются только целочисленные значения
     path('<int:pk>', AdvertisementDetail.as_view(), name='ad_detail'),
-    path('ad_create', AdCreate.as_view(), name='create_advertisement'),
-    path('ad_update', AdUpdate.as_view(), name='update_advertisement')
+
+    path('create', AdCreate.as_view(), name='create_advertisement'),
+    path('edit/<int:pk>', AdUpdate.as_view(), name='update_advertisement'),
+    path('delete/<int:pk>', AdDelete.as_view(), name='delete_advertisement'),
     ]
